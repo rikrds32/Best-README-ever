dab7e1439c562ff296b0da5f3414bb58e0cee576
Ricardo Salazar <no-replay>
2021-06-28 22:08:25 -0500

    add gitignore add index file ,json package ,lock json pkg

---
+++ Develop/index.js
@@ -0,0 +1,82 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+var fs = require("fs");
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "Peoject title?",
+    "Description?",
+    "Installation instructions?",
+    "Usage information?",
+    "Contribution guidelines?",
+    "Test instructions? ",
+    "Choose license for application",
+    "Github User Name?",
+    "e-mail address"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("Succes")
+    );
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "title"
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "description"
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "installation instructions"
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "Usage information"
+        },
+        {
+            type: "input",
+            message: questions[4],
+            name: "Contribution guidelines"
+        },
+        {
+            type: "input",
+            message: questions[5],
+            name: "Test instructions"
+        },
+        {
+            type: "input",
+            message: questions[6],
+            name: "Choose license for application"
+        },
+        {
+            type: "input",
+            message: questions[7],
+            name: "Github User Name"
+        },
+        {
+            type: "input",
+            message: questions[8],
+            name: "e-mail address"
+        },
+    ])
+    .then((response) =>
+    writeToFile("README.md", generateMarkdown(response))
+    );
+}
+
+// Function call to initialize app
+init();
